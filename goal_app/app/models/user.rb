bfca8f1b9a64e163c64c34c507453ee6c0d46537
Chris Joo <chrisj1225@gmail.com>
2021-04-07 16:53:28 -0400

    Finished user model specs

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -1,24 +1,42 @@
 class User < ApplicationRecord
 
+    validates :username, :password_digest, :session_token, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
     after_initialize :ensure_session_token
 
     attr_reader :password
 
     def password=(password)
-        self.password_digest = BCrypt::Password.create(password)
         # set inst. var @password to password
         @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def is_password?(password)
-
+        # takes in password input (string)
+        # called on user instance
+        # calls BCrypt::Password.new on self.password_digest
+        # stores in variable
+        # calls BCrypt method: .is_password?(password) on variable
+        BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
     def self.find_by_credentials(username, password)
+        # takes in username and password
+        # called on User class (class method)
+        # find user instance w/ matching username
+        # checks if password is user instance's password
+        # returns user instance
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
     end
 
     def ensure_session_token
-        # 
         self.session_token ||= SecureRandom.urlsafe_base64
     end
 
--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    subject(:user1) do
+        FactoryBot.create(:user, 
+        username: 'john', 
+        password: 'password')
+    end
 
     it {should validate_presence_of(:username)}
     it {should validate_presence_of(:password_digest)}
     it {should validate_presence_of(:session_token)}
-    it {should validate_length_of(:password_digest).is_at_least(6)}
+    it {should validate_length_of(:password).is_at_least(6)}
 
     describe '#password=(password)' do
-        let(:user1) { create(:user) }
 
         it 'password_digest should not be nil' do
             expect(user1.password_digest).to_not be nil
@@ -20,14 +24,42 @@ RSpec.describe User, type: :model do
 
     end
 
-    describe '#is_password?(password)'
+    describe '#is_password?(password)' do
+
+        context 'with correct password' do
+            it 'return true' do
+                expect(user1.is_password?('password')).to be true
+            end
+        end
+
+        context 'with incorrect password' do
+            it 'return false' do
+                expect(user1.is_password?('password123')).to be false
+            end
+        end
+    end
 
 
-    describe '::find_by_credentials'
+    describe '::find_by_credentials' do
+        # subject(:user2) { User.create!(username: 'john', password: 'password') }
+        # subject(:user) { FactoryBot.create(:user, username: 'john', password: 'password') }
 
+        before { user1.save! }
+        
+        context 'with matching credentials' do
+            it 'returns the matching user' do
+                expect(User.find_by_credentials('john', 'password')).to eq(user1) 
+            end
+        end
+
+        context 'without matching credentials' do
+            it 'returns nil' do
+                expect(User.find_by_credentials('john', 'psasword')).to be nil 
+            end
+        end
+    end
 
     describe '#ensure_session_token' do
-        let(:user1) { create(:user) }
 
         it 'ensures user has a session token' do
             expect(user1.session_token).to_not be nil
@@ -37,7 +69,6 @@ RSpec.describe User, type: :model do
 
 
     describe '#reset_session_token!' do
-        let(:user1) { create(:user) }
 
         it 'resets users session_token' do
             old_session_token = user1.session_token
@@ -45,7 +76,7 @@ RSpec.describe User, type: :model do
             expect(old_session_token).to_not eq(user1.session_token)
         end
 
-        it 'percists new token_session to the DB' do
+        it 'persists new token_session to the DB' do
             user1.reset_session_token!
             expect(user1).to eq(User.last)
         end
